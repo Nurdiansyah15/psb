75d18db4631d9fe3b0315f2b447ed90b5050a7ab
Mohammad Mukhlisin <mmgrup17@gmail.com>
2023-03-29 02:50:26 +0700

    template

--- resources/views/template/partials/footeruser.blade.php
+++ resources/views/template/partials/footeruser.blade.php
@@ -1,9 +1,12 @@
 <footer>
-
-    <div class="container mt-5">
+    <div class="container">
         <div class="col-lg-12">
-            <p>Dibuat dengan &hearts; oleh KGS Devnet. &nbsp;&nbsp;&nbsp; Contact: <a href="http://wa.me/6285280009949" target="_blank" rel="noopener noreferrer">085280009949 (putra)</a> -
-                <a href="http://wa.me/6285280009930" target="_blank" rel="noopener noreferrer">085280009930 (putri)</a></p>
+            <p>Dibuat dengan &hearts; oleh KGS Devnet. &nbsp;&nbsp;&nbsp; Contact: <a
+                    href="http://wa.me/6285280009949" target="_blank" rel="noopener noreferrer">085280009949
+                    (putra)</a> -
+                <a href="http://wa.me/6285280009930" target="_blank" rel="noopener noreferrer">085280009930
+                    (putri)</a></a>
+            </p>
         </div>
     </div>
 </footer>
