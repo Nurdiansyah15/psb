895f9fbfea53dfdab6009def23412baaf61ad9ec
Mohammad Mukhlisin <mmgrup17@gmail.com>
2023-03-30 14:00:00 +0700

    update

--- resources/views/register/endregister.blade.php
+++ resources/views/register/endregister.blade.php
@@ -1,13 +1,9 @@
 @extends("template.user")
 @section("content")
-<div class="text-center" style="margin-top:13%;margin-bottom:15%">
-    @foreach ($data as $row =>$r )
-        @if($r['id']==$id)
+<div class="text-center" style="margin-top:10%;margin-bottom:10%">
 
-            <h5 class="btn" style="background:#146C94;color:#fff;">Nomor Registrasi Anda : {{$r['no_regis']}}</h5>
+            <h5 class="btn" style="background:#146C94;color:#fff;">Nomor Registrasi Anda : {{$data['no_regis']}}</h5>
 
-        @endif
-    @endforeach
     <p class="text-danger">*catatlah agar tidak lupa!</p>
 </div>
 <div class="" style="margin-bottom: 200px"></div>
